a44fa2933905491da9646c4db92eded3b3bcae9d
Vins <sharmavins23@gmail.com>
2020-08-13 19:30:15 -0400

    🎉 initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// Imports
+import React from "react";
+import ReactDOM from "react-dom";
+// Components
+
+// Styles
+
+// Main container
+function App() {
+    // Render
+    return <div></div>;
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
