5ceb9cafc88ef489bbf4200c519548b3816ba73b
Vins <sharmavins23@gmail.com>
2020-08-13 19:52:34 -0400

    🔨 added basic framework

---
+++ components/Title.js
@@ -0,0 +1,12 @@
+// Imports
+import React from "react";
+// Styles
+import styles from "../styles/Title.css";
+
+// Top level page title
+function Title() {
+    // Render
+    return <div></div>;
+}
+
+export default Title;
