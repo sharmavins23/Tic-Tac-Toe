8880b4dcbfbbafcde4dfad5dc953d59f0e440fa4
Vins <sharmavins23@gmail.com>
2020-08-13 20:20:47 -0400

    🎨 added title area

--- components/Title.js
+++ components/Title.js
@@ -6,7 +6,23 @@ import styles from "../styles/Title.css";
 // Top level page title
 function Title() {
     // Render
-    return <div></div>;
+    return (
+        <div>
+            <h1>
+                <mark class="blue">TIC-</mark>
+                <mark class="orange">TAC-</mark>
+                <mark class="blue">TOE</mark>
+            </h1>
+
+            <p>
+                A small React.js tic-tac-toe game created by{" "}
+                <a href="https://github.com/sharmavins23/tic-tac-toe">
+                    @sharmavins23
+                </a>
+                .
+            </p>
+        </div>
+    );
 }
 
 export default Title;
--- index.js
+++ index.js
@@ -2,13 +2,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // Components
+import Title from "./components/Title";
 
 // Styles
 
 // Main container
 function App() {
     // Render
-    return <div></div>;
+    return (
+        <div>
+            <Title />
+        </div>
+    );
 }
 
 ReactDOM.render(<App />, document.getElementById("app"));
