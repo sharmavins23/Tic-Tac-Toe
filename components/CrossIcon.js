582cf7a7ce43112d30d8638682f8ca8b666ad385
Vins <sharmavins23@gmail.com>
2020-08-28 09:50:41 -0400

    🚀 added props value for color

--- components/CrossIcon.js
+++ components/CrossIcon.js
@@ -6,9 +6,9 @@ function CrossIcon(props) {
     // Render
     return (
         <svg
+            class={props.color}
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
-            fill="black"
             width="48px"
             height="48px">
             <path d="M0 0h24v24H0z" fill="none" />
