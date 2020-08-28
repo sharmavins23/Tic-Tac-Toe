98f00126792d5f4f4a2a06ff58a7b4db1e2b3d3e
Vins <sharmavins23@gmail.com>
2020-08-28 09:36:51 -0400

    🎨 added SVG icons as components

---
+++ components/BlankIcon.js
@@ -0,0 +1,19 @@
+// Imports
+import React from "react";
+
+// Blank Icon
+function BlankIcon(props) {
+    // Render
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="black"
+            width="48px"
+            height="48px">
+            <path d="M0 0h24v24H0z" fill="none" />
+        </svg>
+    );
+}
+
+export default BlankIcon;
